0e2fa6f2f0979aea1821b082ca9db5340d5006ab
SiCannon <simon.cannon@gmail.com>
2016-01-30 11:06:31 +0000

    Added a test test project in the correct location

---
+++ test/Uku.Database.Tests/ModelTests/AlbumTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Uku.Database.Tests.ModelTests
+{
+    public class AlbumTests
+    {
+        [Fact]
+        public void Can_Create_Album()
+        {
+            Assert.Equal(5, 2 + 3);
+        }
+    }
+}
---
+++ test/Uku.Database.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Uku.Database.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Uku.Database.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("521825e7-b53a-49d4-9067-2ae4cb2fcdeb")]
