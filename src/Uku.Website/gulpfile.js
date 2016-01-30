a7ef0f12d58d0b56d338e8c0cf264fe46496e118
SiCannon <simon.cannon@gmail.com>
2016-01-30 17:58:49 +0000

    Added bootstrap less

--- src/Uku.Website/gulpfile.js
+++ src/Uku.Website/gulpfile.js
@@ -1,8 +1,9 @@
-﻿/// <binding AfterBuild='min' Clean='clean' ProjectOpened='watcher' />
+﻿/// <binding AfterBuild='less' Clean='clean' ProjectOpened='less-watcher, bootstrap-less-watcher' />
 "use strict";
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
+    del = require("del"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
@@ -21,6 +22,10 @@ paths.lessOutput = paths.webroot + "css/";
 paths.lessCss = paths.lessOutput + "**/*.css";
 paths.concatJsDest = paths.webroot + "js/all.min.js";
 paths.concatCssDest = paths.webroot + "css/all.min.css";
+paths.bootstrap_less_root = paths.webroot + "lib/bootstrap-less-only/less/";
+paths.bootstrap_less_mainfile = paths.bootstrap_less_root + "bootstrap.less";
+paths.bootstrap_less_allfiles = paths.bootstrap_less_root + "**/*.less";
+paths.bootstrap_less_output = paths.bootstrap_less_root;
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
@@ -31,7 +36,7 @@ gulp.task("clean:css", function (cb) {
 });
 
 gulp.task("clean:less", function (cb) {
-    rimraf(paths.lessCss, cb);
+    del(paths.lessCss);
 });
 
 gulp.task("clean", ["clean:js", "clean:css", "clean:less"]);
@@ -58,6 +63,16 @@ gulp.task("min:css", ["less"], function () {
 
 gulp.task("min", ["min:js", "min:css"]);
 
-gulp.task("watcher", function () {
+gulp.task("less-watcher", function () {
     gulp.watch(paths.less, ["less"]);
+});
+
+gulp.task("bootstrap-less", function () {
+    return gulp.src(paths.bootstrap_less_mainfile)
+        .pipe(less())
+        .pipe(gulp.dest(paths.bootstrap_less_output));
+});
+
+gulp.task("bootstrap-less-watcher", function () {
+    gulp.watch(paths.bootstrap_less_allfiles, ["bootstrap-less"]);
 });
